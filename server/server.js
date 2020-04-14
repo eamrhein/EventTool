c15a362ef3a057a18e328bb8fa161294f76040a3
Eric Amrhein <eamrhein@protonmail.ch>
2020-04-14 11:40:55 -0700

    fixing deployment issues

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/client/build/index.html"));
   });
 }

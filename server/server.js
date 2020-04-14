d7d229ae6b047cceaa51292ddb1e41eef0fed35e
Eric Amrhein <eamrhein@protonmail.ch>
2020-04-13 18:50:42 -0700

    fixes

--- server/server.js
+++ server/server.js
@@ -28,6 +28,9 @@ app.use(
 );
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  });
 }
 
 if (!db) {

884fa95bc55337e82e8536befcb71ef0e59f8bb4
Eric Amrhein <eamrhein@protonmail.ch>
2020-04-14 11:44:36 -0700

    test heroku

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+const password = "jd0bxBlU9AdMQQaF";
+
+module.exports = {
+  secretOrkey: "hellodolly123456",
+  mongoURI: `mongodb+srv://EventTool:${password}@cluster0-eu8re.mongodb.net/EventTool?retryWrites=true&w=majority`
+};

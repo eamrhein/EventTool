e6f7b470b2bd82808a73623f2e79c3933eba4b2a
Eric Amrhein <eamrhein@protonmail.ch>
2020-04-22 00:24:29 -0700

    test

---
+++ client/build/precache-manifest.c950fcb9502a9bde711bb8999724473f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311b0b5f54a310a378ccd3dda286201f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d38ebc2fedeca9e26e69",
+    "url": "/static/css/main.6218043c.chunk.css"
+  },
+  {
+    "revision": "c83a05da5585d9ca1675",
+    "url": "/static/js/2.b938dd33.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.b938dd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38ebc2fedeca9e26e69",
+    "url": "/static/js/main.afa7cb40.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);

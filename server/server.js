8ccd8c08eaa77d35cb4449d3160b1f93011e4282
Eric Amrhein <eamrhein@protonmail.ch>
2020-05-05 17:33:50 -0700

    fixes redirect

--- server/server.js
+++ server/server.js
@@ -31,13 +31,13 @@ let redirector = require("redirect-https")({
   body: "<!-- Hello Developer! Please use HTTPS instead: {{ URL }} -->",
 });
 app.use("/", redirector);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
 app.use(
   "/graphql",
   expressGraphQl((req) => {
